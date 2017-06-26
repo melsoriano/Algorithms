ad2993045ce0a87bb20aa01e364841a19bac0403
mellaniesoriano <msoriano808@gmail.com>
2017-06-26 11:38:57 -1000

    nested for/loop, working on other ways

--- insertion.js
+++ insertion.js
@@ -2,7 +2,6 @@ var unsortedArr = [9, 3, 87, 4, 1, 7, 10];
 
 
 function insertion(array) {
-
   for(var i = 1; i < array.length; i++) {
     var temp = array[i];
     for(var k = i - 1; k >= 0 && array[k] > temp; k--) {
